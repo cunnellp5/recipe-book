8fe92c54a2d2d80ec48713a24d19011c618b4170
pcunnell <philip.cunnell@clickfox.com>
2019-04-18 12:06:08 -0600

    modified recipe interface and added ingredient array to detail view

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,17 @@
+import { IIngredient } from '../shared/ingredient.model';
+
 export class IRecipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: IIngredient[];
 
   constructor(
-    name: string, description: string, imagePath: string
+    name: string, description: string, imagePath: string, ingredients: IIngredient[]
   ) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,31 @@
 import { IRecipe } from './recipe.model';
 import { Output, EventEmitter } from '@angular/core';
+import { IIngredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
   @Output() recipeSelected = new EventEmitter<IRecipe>();
 
   private recipes: IRecipe[] = [
-    new IRecipe('A test recipe', 'This is simplay a test', 'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg'),
+    new IRecipe(
+      'A test recipe',
+      'This is simplay a test',
+      'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg',
+      [
+        new IIngredient('poop', 1),
+        new IIngredient('stuff', 2)
+      ]
+    ),
     // tslint:disable-next-line:max-line-length
-    new IRecipe('Another test recipe', 'This is simplay a test', 'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg')
+    new IRecipe(
+      'Another test recipe',
+      'This is simplay a tickls',
+      'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg',
+      [
+        new IIngredient('junk', 2),
+        new IIngredient('garbage', 2)
+      ]
+    )
   ];
 
   getRecipes() {
