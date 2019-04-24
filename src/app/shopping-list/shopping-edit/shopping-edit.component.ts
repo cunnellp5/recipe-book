96e766ed5b7b7dc443d3144244e5cd527e770bef
pcunnell <philip.cunnell@clickfox.com>
2019-04-24 08:19:24 -0600

    adding basic form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,10 @@
 import { ShoppingListService } from './../shopping-list.service';
 import {
   Component,
-  OnInit,
-  ViewChild,
-  ElementRef
+  OnInit
 } from '@angular/core';
 import { IIngredient } from 'src/app/shared/ingredient.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -13,18 +12,14 @@ import { IIngredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
-
   constructor(private _shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingredientName = this.nameInputRef.nativeElement.value;
-    const ingredientAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new IIngredient(ingredientName, ingredientAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new IIngredient(value.name, value.amount);
     this._shoppingListService.addIngredient(newIngredient);
   }
 
