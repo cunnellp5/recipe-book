4051e5e276dcb8bf0d5223231cbe5bc5a4f0283b
pcunnell <philip.cunnell@clickfox.com>
2019-05-20 12:56:13 -0600

    adding params to new httpclient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,4 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  // HttpHeaders,
+  HttpParams,
+  HttpRequest
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from './../auth/auth.service';
 import { RecipeService } from '../recipes/recipe.service';
@@ -15,19 +20,34 @@ export class DataStorageService {
 
   storeRecipes() {
     const token = this.authService.getToken();
-    return this.httpClient.put(
-      'https://recipe-book-ba3e8.firebaseio.com/recipes.json?auth=' + token,
-      this._recipeService.getRecipes(), {
-        observe: 'body'
+    // const headers = new HttpHeaders().set('Authorization', 'Bearer sometoken');
+
+    // return this.httpClient.put(
+    //   'https://recipe-book-ba3e8.firebaseio.com/recipes.json' + token,
+    //   this._recipeService.getRecipes(), {
+    //     observe: 'body',
+    //     params: new HttpParams().set('auth', token)
+    //     // headers: headers
+    //   }
+    // );
+
+    const req = new HttpRequest(
+      'PUT',
+      'https://recipe-book-ba3e8.firebaseio.com/recipes.json',
+      this._recipeService.getRecipes(),
+      {
+        reportProgress: true,
+        params: new HttpParams().set('auth', token)
       }
     );
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
     const token = this.authService.getToken();
-    return this.httpClient.get<IRecipe[]>('https://recipe-book-ba3e8.firebaseio.com/recipes.json?auth=' + token, {
+    return this.httpClient.get<IRecipe[]>('https://recipe-book-ba3e8.firebaseio.com/recipes.json' + token, {
       observe: 'body',
-      responseType: 'json'
+      params: new HttpParams().set('suth', token)
     })
       .pipe(
         map(
