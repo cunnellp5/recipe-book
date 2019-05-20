61c35e429f419c5038f329c7fa177c40148aa5c4
pcunnell <philip.cunnell@clickfox.com>
2019-05-20 13:45:15 -0600

    adding a payload

---
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,21 @@
+import { IIngredient } from '../../shared/ingredient.model';
+import * as ShoppingListActions from './shopping-list.actions';
+
+const initialState = {
+  ingredients: [
+    new IIngredient('apples', 5, false),
+    new IIngredient('bananas', 11, false)
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+
+  }
+  return state;
+}
