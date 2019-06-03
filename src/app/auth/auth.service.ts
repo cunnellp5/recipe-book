371e89d694ba9b4984f336adf78f194cc0ebf9ed
pcunnell <philip.cunnell@clickfox.com>
2019-06-03 08:58:23 -0600

    moving auth token to state

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,24 @@
-import * as firebase from 'firebase';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import * as fromApp from '../store/app.reducers';
+import * as AuthActions from './store/auth.actions';
 
 @Injectable()
 export class AuthService {
-  token: string;
-
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private store: Store<fromApp.AppState>
+  ) { }
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(res => this.signinUser(email, password))
+      .then(
+        user => {
+          this.store.dispatch(new AuthActions.Signup());
+        }
+      )
       .catch(
         error => console.log(error)
       );
@@ -20,10 +28,13 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         res => {
+          this.store.dispatch(new AuthActions.Signup());
           this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
             .then(
-              (token: string) => this.token = token
+              (token: string) => {
+                this.store.dispatch(new AuthActions.SetToken(token));
+              }
             );
         }
       )
@@ -32,21 +43,9 @@ export class AuthService {
       );
   }
 
-  getToken() {
-    firebase.auth().currentUser.getIdToken()
-      .then(
-        (token: string) => this.token = token
-      );
-    return this.token;
-  }
-
-  isAuthenticated() {
-    return this.token != null;
-  }
-
   logout() {
     firebase.auth().signOut();
-    this.token = null;
+    this.store.dispatch(new AuthActions.Logout());
     this.router.navigate(['/login']);
   }
 
