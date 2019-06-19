dc51345b486aea8e1195edd2ff597014c74c4db3
pcunnell <philip.cunnell@clickfox.com>
2019-06-19 15:07:11 -0600

    adjusting auth from state

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -5,25 +5,16 @@ import { Injectable } from '@angular/core';
 import * as fromApp from '../store/app.reducers';
 import * as fromAuth from '../auth/store/auth.reducers';
 import { Store } from '@ngrx/store';
-import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private store: Store<fromApp.AppState>) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    // const copiedReq = req.clone({
-    //   params: req.params.set('auth')
-    // });
-
     console.log('intercepted!', req);
     return this.store.select('auth')
       .switchMap((authState: fromAuth.State) => {
         const copiedReq = req.clone({ params: req.params.set('auth', authState.token) });
         return next.handle(copiedReq);
       });
-
-    // next.handle(copiedReq);
   }
-
 }
