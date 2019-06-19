089f2583b403e6a71050818b6301923ca8efb7a3
pcunnell <philip.cunnell@clickfox.com>
2019-06-19 14:44:51 -0600

    adding state tokens

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -2,19 +2,28 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
-import { AuthService } from './../auth/auth.service';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
+import { Store } from '@ngrx/store';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) { }
+  constructor(private store: Store<fromApp.AppState>) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const copiedReq = req.clone({
-      params: req.params.set('auth', this.authService.getToken())
-    });
+    // const copiedReq = req.clone({
+    //   params: req.params.set('auth')
+    // });
 
     console.log('intercepted!', req);
-    return next.handle(copiedReq);
+    return this.store.select('auth')
+      .switchMap((authState: fromAuth.State) => {
+        const copiedReq = req.clone({ params: req.params.set('auth', authState.token) });
+        return next.handle(copiedReq);
+      });
+
+    // next.handle(copiedReq);
   }
 
 }
